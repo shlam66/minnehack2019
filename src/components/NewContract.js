c073d9090a853ab219d370ccb17f156a63dfefaf
lorim007 <lorim007@umn.edu>
2019-02-03 12:57:32 -0600

    Contract submit finished

--- src/components/NewContract.js
+++ src/components/NewContract.js
@@ -1,22 +1,143 @@
-import React from 'react'
-import "./Components.css" 
-import {Link} from "react-router-dom"
+import React, { Component } from 'react'
+import api from '../api'
+import './Components.css'
+import { Link } from 'react-router-dom'
+import { Dropdown } from 'react-bulma-components/full'
+import { Form, Button } from 'react-bulma-components/full'
 
-import TextBox from './TextBox'
+class NewContract extends Component {
+  state = {
+    name: '',
+    email: '',
+    strain: '',
+    price: 0,
+    acres: 0
+  }
 
-import "./Components.css"
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
 
-const NewContract = () => (
-    <div class="personalLogs">
-        <h1 className="title">Purpose a new contract</h1>
+  onChange = strain => {
+    this.setState({ strain })
+  }
 
-        <TextBox className="contract-new-field" placeholder="Johny Appleseed">Name</TextBox>
-        <TextBox className="contract-new-field" type="number" placeholder={100}>Land (Acres)</TextBox>
-        <TextBox className="contract-new-field" type="number" paceholder={20000}>Price</TextBox>
-        <TextBox className="contract-new-field" placeholder="johnyappleseed@apple.com">Email</TextBox>
-        <br></br>
-        <Link to="/contracts"><button class="button is-primary is-rounded" type="submit">Submit Contract</button></Link>
-    </div>
-);
+  submit = () => {
+    const entry = {
+        lister:{
+            name: this.state.name,
+            avatar_id: 1,
+            email: this.state.email
+        },
+        farm:{
+            acres: this.state.acres,
+            state: "MN",
+            county: "Anoka"
+        },
+        price: this.state.price,
+        strain: {
+            id:1,
+            name: this.state.strain,
+            //These values can be imporved upon (obviously) with using an api
+            //or creating our own database for the strands that we support
+            weight: Math.floor((Math.random() * 10) + 1),
+            thc_content: 0.0,
+            cbd_content: 20,
+            planting_time: "Early October"
+        }
+    }
+    api.post('/contracts', entry).then(res => {
+      this.props.history.push('/contracts')
+    })
+  }
+
+  render() {
+    return (
+      <div className="personalLogs">
+        <h1 className="title">Create a new log</h1>
+
+        <Form.Field>
+          <Form.Label>Name:</Form.Label>
+          <Form.Control>
+            <Form.Input
+              type="text"
+              onChange={this.handleInputChange}
+              name="name"
+              required
+              value={this.state.name}
+            />
+          </Form.Control>
+        </Form.Field>
+
+        <Form.Field>
+          <Form.Label>Email:</Form.Label>
+          <Form.Control>
+            <Form.Input
+              type="text"
+              onChange={this.handleInputChange}
+              name="email"
+              required
+              value={this.state.email}
+            />
+          </Form.Control>
+        </Form.Field>
+
+        <Form.Field>
+          <Form.Label>Number of Acres:</Form.Label>
+          <Form.Control>
+            <Form.Input
+              type="number"
+              onChange={this.handleInputChange}
+              name="acres"
+              required
+              value={this.state.acres}
+            />
+          </Form.Control>
+        </Form.Field>
+
+        <Form.Field>
+          <Form.Label>Select the Strain:</Form.Label>
+          <Form.Control>
+            <Dropdown value={this.state.strain} onChange={this.onChange}>
+              <Dropdown.Item value="Elektra">Elektra</Dropdown.Item>
+              <Dropdown.Item value="Cherry Wine">Cherry Wine</Dropdown.Item>
+              <Dropdown.Item value="Suver Haze">Suver Haze</Dropdown.Item>
+              <Dropdown.Item value="Cobbler">Cobbler</Dropdown.Item>
+            </Dropdown>
+          </Form.Control>
+        </Form.Field>
+
+        <Form.Field>
+          <Form.Label>Price</Form.Label>
+          <Form.Control>
+            <Form.Input
+              type="number"
+              onChange={this.handleInputChange}
+              name="price"
+              required
+              value={this.state.price}
+            />
+          </Form.Control>
+        </Form.Field>
+
+        
+        <Form.Field kind="group">
+          <Form.Control>
+            <Link to="/contracts/">
+              <Button type="primary" color="primary" onClick={this.submit}>
+                Submit
+              </Button>
+            </Link>
+          </Form.Control>
+          <Form.Control>
+            <Link to="/contracts/">
+              <Button>Cancel</Button>
+            </Link>
+          </Form.Control>
+        </Form.Field>
+      </div>
+    )
+  }
+}
 
 export default NewContract
