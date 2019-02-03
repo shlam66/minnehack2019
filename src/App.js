34c143e8790bb3121893c7602c4bf7f1e200e942
Marcus Virginia <marcusvirg345@gmail.com>
2019-02-03 00:55:36 -0600

    Finished app component

--- src/App.js
+++ src/App.js
@@ -10,24 +10,25 @@ import Logs from './components/Logs'
 import NewLog from './components/NewLog'
 import DataSets from './components/DataSets'
 
+import './App.css'
+
 const AppRouter = () => (
   <Router>
-    <div style={{backgroundColor: "#FF0000"}}>
-      <h1>HEMPTATION biiiiiiitch</h1>
+    <div>
       <Navbar />
+      <div className="container hemp-container">
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about/" component={About} />
+        <Route path="/contracts/" exact component={Contracts} />
+        <Route path="/contracts/new" component={NewContract} />
 
-      <Route path="/" exact component={HomePage} />
-      <Route path="/about/" component={About} />
-
-      <Route path="/contracts/" component={Contracts} />
-      <Route path="/newcontract/" component={NewContract} />
-
-      <Route path="/logs/" component={Logs} />
-      <Route path="/newlog/" component={NewLog} />
-      <Route path="/data/" component={DataSets} />
+        <Route path="/logs/" exact component={Logs} />
+        <Route path="/logs/new" component={NewLog} />
+        <Route path="/data/" component={DataSets} />
+      </div>
     </div>
   </Router>
-);
+)
 
 export default class App extends Component {
   render() {
