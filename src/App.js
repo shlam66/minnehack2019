2d3001f6172092d55486c64415e003ae5a111ea4
Marcus Virginia <marcusvirg345@gmail.com>
2019-02-02 22:07:51 -0600

    Initial develop commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router'
+
+import Navbar from './components/Navbar'
+
+export default class App extends Component {
+  render() {
+    return <div />
+  }
+}
