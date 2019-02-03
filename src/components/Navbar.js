1b55b7883e58af6eb14ef1327535c0509f180b34
Marcus Virginia <marcusvirg345@gmail.com>
2019-02-03 00:05:24 -0600

    Added bulma styles and used bulma navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,71 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => (
-    <nav style={{backgroundColor: "#02B45B"}}>
-        <ul>
-            <li>
-                <Link to="/">Main Page</Link>
-            </li>
-            <li>
-                <Link to="/about/">Why Hemp</Link>
-            </li>
-            <li>
-                <Link to="/contracts/">Contracts</Link>
-            </li>
-            <li>
-                <Link to="/logging/">Logging</Link>
-            </li>
-            <li>
-                <Link to="/data/">Data Sets</Link>
-            </li>
-        </ul>
-    </nav>
-);
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img
+          src="https://bulma.io/images/bulma-logo.png"
+          width="112"
+          height="28"
+          alt="Hemptation Logo"
+        />
+      </a>
+
+      <button
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </button>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <Link to="/" class="navbar-item">
+          Home
+        </Link>
+
+        <Link to="/about" class="navbar-item">
+          Why Hemp?
+        </Link>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <Link to="#" class="navbar-link">
+            More
+          </Link>
+
+          <div class="navbar-dropdown">
+            <Link to="/" class="navbar-item">
+              About
+            </Link>
+            <Link to="/" class="navbar-item">
+              Jobs
+            </Link>
+            <Link to="/" class="navbar-item">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <button class="button is-primary">
+              <strong>Sign up</strong>
+            </button>
+            <button class="button is-light">Log in</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+)
 
 export default Navbar
